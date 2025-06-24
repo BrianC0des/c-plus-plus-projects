8d8711bfd702df6daf5033870dece2934a01bb21
BrianC0des <bryansaavedrab272@gmail.com>
2025-06-24 09:34:40 +0800

    added new files

---
+++ projects/simple_projects/helloworld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+
+    cout << "Enter a number: " << endl;
+    cin >> num;
+
+    if (num > 12)
+        {
+            cout << "The number is greater than 12 " << endl; 
+        }
+    else
+        {
+            cout << "The number is less than 12" << endl;
+        }
+
+return 0;
+}
