5069ed9c7221cbfc9c4a330f752ec928b6e915e3
BrianC0des <bryansaavedrab272@gmail.com>
2025-06-24 09:56:56 +0800

    added new lines of code

--- projects/simple_projects/helloworld.cpp
+++ projects/simple_projects/helloworld.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int num;
 
     cout << "Enter a number: " << endl;
     cin >> num;
 
     if (num > 12)
-        {
-            cout << "The number is greater than 12 " << endl; 
-        }
-    else
-        {
-            cout << "The number is less than 12" << endl;
-        }
+    {
+        cout << "The number is greater than 12 " << endl;
+    }
+    else if (num == 12)
+    {
+        cout << "The number is equal to 12" << endl;
+    }
 
-return 0;
+    else if (num == 22)
+    {
+        cout << "The number is 22" << endl;
+    }
+
+    else
+    {
+        cout << "The number is greater than 12" << endl;
+    }
+
+    return 0;
 }
