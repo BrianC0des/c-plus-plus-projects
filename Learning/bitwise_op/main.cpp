bff31108c31d1992b3a5b7a274b1da683d3f5238
BrianC0des <bryansaavedrab272@gmail.com>
2025-10-26 20:39:40 +0800

    added files

---
+++ Learning/bitwise_op/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() 
+{
+    int x, y ,z;
+    x = 6 ;
+    y = 12;
+    z = 0;
+
+    z = x & y; cout << z <<endl;
+    z = x | y; cout << z <<endl;
+    z = x << x; cout << z <<endl;
+    z = x >> x; cout << z <<endl;
+
+     
+
+ }  
--- Learning/main.cpp
+++ Learning/main.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int main() 
-{
-    
-    vector<string> fruits = {"Banana","Apple","Strawberry","Kiwi","Pineapple"};
-    fruits.push_back("Blueberry");    
-    int len = fruits.size();
-    for(int i = 0; i < len; i++){
-        cout << "Index:"<< i << fruits[i] <<endl;
 
-        
-    }
+
+int main(){
+
+    string myString = "Hello";
+    cout << myString[myString.size() - 1];
+
+
+
+
 
 
 
