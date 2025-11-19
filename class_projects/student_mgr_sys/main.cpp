da25cbda3eff5e139a579237f6a746edd8adfd9a
BrianC0des <bryansaavedrab272@gmail.com>
2025-11-19 14:14:01 +0800

    added new changes

--- class_projects/student_mgr_sys/main.cpp
+++ class_projects/student_mgr_sys/main.cpp
@@ -1,20 +1,99 @@
+#include <fstream>
 #include <iostream>
 using namespace std;
 
 
 
-void newStudent
-
+void newStudents(string name, float marks);
+void viewStudent();
+char menu();
 
 int main(){
+    char repeat;
+    string name;
+    float marks;
 
+    do{
 
-
-
-
-
-
-
-
+        switch (menu())
+        {
+            case '1':
+                    
+                    cin.ignore();
+                    cout << "Enter a name: ";
+                    getline(cin, name);
+                    cout << "Enter a mark: ";
+                    cin >> marks;
+                    newStudents(name, marks);
+                
+                break;
+            case '2':
+                system("clear");
+                cout << "================Students================="<<endl;
+                viewStudent(); 
+        
+                break;
+            default:
+                break;
+            
+        }
+        
+        cout << "Enter another? [Y/n]: ";
+        cin >> repeat;
+        
+        
+    }while(repeat == 'Y' || repeat == 'y');
+    
+    
     return 0;
 }
+
+
+
+void newStudents(string name, float marks)
+{
+
+    fstream studentFile("Studentlog.txt", ios::in | ios::app);
+    
+    if(!studentFile){
+        cout << "Error opening file!";
+    }
+
+    studentFile << "Name: "<<name<<endl;
+    studentFile << "Mark: "<<marks;
+    studentFile <<endl<<endl;
+    
+    studentFile.close();
+    cout << "Added Successfully!"<<endl;
+    
+}
+
+
+void viewStudent(){
+        ifstream studentFile("Studentlog.txt");
+        
+        if(studentFile){
+            string line;
+            while(getline(studentFile, line)){
+                cout << line <<endl;
+            }
+            studentFile.close();   
+
+        }
+}
+
+
+
+char menu(){
+    char choice;
+    system("clear");
+    cout << "========= Menu ==========" <<endl;
+    cout << "[1] Add Student" <<endl;
+    cout << "[2] View Students" <<endl;
+    cout << "[3] clear" <<endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+    return choice;
+
+}
+
