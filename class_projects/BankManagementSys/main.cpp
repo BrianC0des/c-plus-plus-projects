1cb38ce740be4258ad52b31d713d85b8b4ba748e
BrianC0des <bryansaavedrab272@gmail.com>
2025-11-24 06:54:09 +0800

    added receipt infos

--- class_projects/BankManagementSys/main.cpp
+++ class_projects/BankManagementSys/main.cpp
@@ -1,278 +1,296 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <limits>
 using namespace std;
 
+// Bryan P. Saavedra
+// BSCS 1B
+// HEllO
+// global balance
 double balance;
 
+// my functions
 
-
+void withdrawReceipt();
 double getBalance(int userId);
 void getAccount(int userId, int &accID, int &accPIN);
-char authenticatorMenu();
-void createAccount(int userId, int pin, double balance );
+int authenticatorMenu();
+void createAccount(int userId, int pin, double balance);
 double depositMoney(double amount, int userId, int pin);
 double withdrawMoney(double amount, int userId, int pin);
-char menu();
+int menu();
 void clearInputBuffer();
 
+// my main function
+int main() {
+  double prevBal;
+  int r;
+  int attempts = 3;
+  char cont;
 
+  do {
+    int choice = authenticatorMenu();
+    if (choice == 1) {
 
-int main(){
-    char r;
-    int attempts = 2;    
-    char cont;
+      do {
 
-    do{   
-        char choice = authenticatorMenu();
-        if(choice == '1'){
-            
-            do{
-                int ID, PIN;
-                int userID, pincode;
-                cout << "Enter ID number: ";
-                cin >> userID;
-                cout << "Enter PINCODE: ";
-                cin >> pincode;
-                    
-                
-                getAccount(userID,ID,PIN);
-                balance = getBalance(userID);
-            
-
-                
-                if( userID == ID && pincode == PIN ){
-                    do{
-                        char choice = menu();
-                        double amount;
-                        switch (choice)
-                        {
-                        case '1':
-                                cout << "Enter amount to deposit: ";
-                                cin >> amount;
-                                balance = depositMoney(amount, userID, pincode);
-                                system("clear");
-                                cout << "Transanction Successful!"<<endl;
-                                cout << "Balance: "<<balance<<endl;
-                                
-                                break;
-                                
-                                case '2':
-                                while(true){
-                                    cout << "Enter amount to withdraw: ";
-                                    cin >> amount;
-                                    if(amount > balance){
-                                        cout << "Insufficient Balance Please Try Again!"<<endl;
-                                        cout << "Balance: "<<balance<<endl;
-                                    }
-                                    else{
-                                        balance = withdrawMoney(amount, userID, pincode);
-                                        system("clear");
-                                        cout << "Transanction Successful!"<<endl;
-                                        cout << "Balance: "<<balance<<endl;
-                                        break;
-                                    }
-                                }
-                                    
-                            break;
-
-                        case '3':
-                            system("clear");
-                            cout <<"Your Balance is: " <<balance<<endl;
-                            break;
-                            
-                            case '4':
-                            system("clear");
-                            cout << "Thank you for using our bank!"<<endl;
-                            return 0;
-
-                        default:
-                            break;
-                        }
-                        cout <<"========== BDO Bank =========="<<endl;
-                        cout << "[1] MENU\n[2] EXIT"<<endl;
-                        cout << "Enter your choice: ";
-                        cin >> r;
-                    }while(r == '1');
-                    cout << "Thank you for using our bank!"<<endl;
-                    return 0;
-                }
-                else if(userID != ID || pincode != ID){
-                    cout << "Please try again!"<<endl;
-                    cout <<"Remaining Attempts: "<<attempts<<endl;
-                    attempts--;
-                }
-                else{
-                    cout << "Invalid Inputs!";
-                    break;
-                }
-                            
-            
-            }while(attempts >= 0);
-            cout << "Account Locked!";
+        int ID, PIN;
+        int userID, pincode;
+        cout << "Enter ID number: ";
+        while (!(cin >> userID)) {
+          cout << "Error! Enter a number: ";
+          clearInputBuffer();
         }
-        else if(choice == '2'){
-            int pin, cpin, userId;
-            cout << "Enter your ID number: ";
-            cin >> userId;
-            while(true){
-                cout << "Enter your pincode: ";
-                cin >> pin;
-                cout << "Confirm your pincode: ";
-                cin >> cpin;
-                if(cpin != pin){
-                    cout << "Pin not match! Please try again!"<<endl;
-
-                }
-                else{
-                    cout << "Enter Initial Balance: ";
-                    cin >> balance;
-                    createAccount(userId, pin, balance);
-                    break;
-                }
-            }    
+        cout << "Enter PINCODE: ";
+        while (!(cin >> pincode)) {
+          cout << "Error! Enter a number: ";
+          clearInputBuffer();
         }
-        cout << "Back to Login Page? [Y/n]: ";
-        cin >> cont;
-    }while(cont == 'y' || cont == 'Y');
-    cout << "Thank you for using our bank!";
-    
-    return 0;
+
+        getAccount(userID, ID, PIN);
+        balance = getBalance(userID);
+
+        if (userID == ID && pincode == PIN) {
+          do {
+            int choice = menu();
+            double amount;
+            switch (choice) {
+            case 1:
+              cout << "Enter amount to deposit: ";
+              while (!(cin >> amount)) {
+                cout << "Error! Enter a number: ";
+                clearInputBuffer();
+                continue;
+              }
+              prevBal = balance;
+              balance = depositMoney(amount, userID, pincode);
+              system("clear");
+              cout << "Transanction Successful!" << endl;
+              cout << "========= Deposit Receipt =========" << endl;
+              cout << "Initial balance: " << prevBal << endl;
+              cout << "Amount Deposited: " << amount << endl;
+              cout << "New Balance: " << balance << endl;
+              cout << endl;
+
+              // cout << "Transanction Successful!"<<endl;
+              // cout << "Balance: "<<balance<<endl;
+
+              break;
+
+            case 2:
+              while (true) {
+                cout << "Enter amount to withdraw: ";
+                while (!(cin >> amount)) {
+                  cout << "Error! Enter a number: ";
+                  clearInputBuffer();
+                  continue;
+                }
+                if (amount > balance) {
+                  cout << "Insufficient Balance Please Try Again!" << endl;
+                  cout << "Balance: " << balance << endl;
+                } else {
+                  prevBal = balance;
+                  balance = withdrawMoney(amount, userID, pincode);
+                  system("clear");
+                  cout << "Transanction Successful!" << endl;
+                  cout << "========= Withdrawal Receipt =========" << endl;
+                  cout << "Initial balance: " << prevBal << endl;
+                  cout << "Amount Withdrawn: " << amount << endl;
+                  cout << "New Balance: " << balance << endl;
+                  cout << endl;
+
+                  break;
+                }
+              }
+
+              break;
+
+            case 3:
+              system("clear");
+              cout << "Your Balance is: " << balance << endl;
+              break;
+
+            case 4:
+              system("clear");
+              cout << "Thank you for using our bank!" << endl;
+              return 0;
+
+            default:
+              break;
+            }
+            cout << "========== BDO Bank ==========" << endl;
+            cout << "[1] MENU\n[2] EXIT" << endl;
+            cout << "Enter your choice: ";
+            while (!(cin >> r)) {
+              cout << "Error! Enter a number: ";
+              clearInputBuffer();
+            }
+          } while (r == 1);
+          cout << "Thank you for using our bank!" << endl;
+          return 0;
+        } else if (userID != ID || pincode != PIN) {
+          cout << "Please try again!" << endl;
+          cout << "Remaining Attempts: " << attempts << endl;
+          attempts--;
+        } else {
+          cout << "Invalid Inputs!";
+          return 0;
+        }
+
+      } while (attempts != 0);
+      cout << "Account Locked!" << endl;
+    } else if (choice == 2) {
+      int pin, cpin, userId;
+      cout << "Enter your ID number: ";
+      cin >> userId;
+      while (true) {
+        cout << "Enter your pincode: ";
+        cin >> pin;
+        cout << "Confirm your pincode: ";
+        cin >> cpin;
+        if (cpin != pin) {
+          cout << "Pin not match! Please try again!" << endl;
+
+        } else {
+          cout << "Enter Initial Balance: ";
+          cin >> balance;
+          createAccount(userId, pin, balance);
+          break;
+        }
+      }
+    } else {
+      cout << "Invalid Input" << endl;
+    }
+    cout << "Back to Login Page? [Y/n]: ";
+    cin >> cont;
+  } while (cont == 'y' || cont == 'Y');
+  cout << "Thank you for using our bank!";
+
+  return 0;
 }
 
-char menu(){
-    char choice;
+// memu func
+int menu() {
+  int choice;
 
-        system("clear");
-        cout << "========== BDO Bank =========="<<endl;
-        cout << "[1] DEPOSIT"<<endl;
-        cout << "[2] WITHDRAW"<<endl;
-        cout << "[3] INQUIRE"<<endl;
-        cout << "[4] EXIT"<<endl;
-        cout <<"Enter transaction: ";
-        cin >> choice;
-        
-        return choice;
-}   
-    
-char authenticatorMenu(){
-    char choice;
-    system("clear");
-    cout << "========== BDO Bank =========="<<endl;
-    cout << "[1] Login"<<endl;
-    cout << "[2] Sign Up"<<endl;
-    cout << "Enter your choice: ";
-    cin >> choice;
-    cout <<endl;
-    system("clear");
-    return choice;
-   
+  system("clear");
+  cout << "========== BDO Bank ==========" << endl;
+  cout << "[1] DEPOSIT" << endl;
+  cout << "[2] WITHDRAW" << endl;
+  cout << "[3] INQUIRE" << endl;
+  cout << "[4] EXIT" << endl;
+  cout << "Enter transaction: ";
+  while (!(cin >> choice)) {
+    cout << "Error! Enter a number: ";
+    clearInputBuffer();
+  }
+
+  return choice;
 }
 
-
-void createAccount(int userId, int pin, double balance){
-    string ext = ".txt";
-    string filename = to_string(userId) + ext; 
-    string filepath = "accountsDatabase/" + filename;
-    fstream studentFile(filepath, ios::out);
-    
-    if(!studentFile){
-        cout << "Error opening file!";
-    }
-
-    studentFile <<"ID : "<<userId<<endl;
-    studentFile <<"PIN : " << pin<<endl;
-    studentFile <<"BALANCE : " << balance<<endl;
-    studentFile <<endl;
-    
-    studentFile.close();
-    system("clear");
-    cout << "Sign Up Successfully!"<<endl;   
-    
+// Login and Sign Up Menu func
+int authenticatorMenu() {
+  int choice;
+  system("clear");
+  cout << "========== BDO Bank ==========" << endl;
+  cout << "[1] Login" << endl;
+  cout << "[2] Sign Up" << endl;
+  cout << "Enter your choice: ";
+  while (!(cin >> choice)) {
+    cout << "Error! Enter a number: ";
+    clearInputBuffer();
+  }
+  cout << endl;
+  system("clear");
+  return choice;
 }
 
-void getAccount(int userId, int &accID, int &accPIN){
-    string ext = ".txt";
-    string filename = to_string(userId) + ext; 
-    string filepath = "accountsDatabase/" + filename;
-    fstream studentFile(filepath, ios :: in);
-    
-    if(!studentFile){
-        cout << "Error opening file!";
-    }
-    string word;
-    
-    studentFile >> word >> word >>accID;
-    for(int i = 1; i <= 1; i++){
-        studentFile >> word >> word >> accPIN;
-    }
-        
-    studentFile.close(); 
-    
+// create acc func
+void createAccount(int userId, int pin, double balance) {
+  string ext = ".txt";
+  string filename = to_string(userId) + ext;
+  fstream studentFile(filename, ios::out);
+
+  studentFile << "ID : " << userId << endl;
+  studentFile << "PIN : " << pin << endl;
+  studentFile << "BALANCE : " << balance << endl;
+  studentFile << endl;
+
+  studentFile.close();
+  system("clear");
+  cout << "Account Created Successfully!" << endl;
 }
 
+// get account func
+void getAccount(int userId, int &accID, int &accPIN) {
+  string ext = ".txt";
+  string filename = to_string(userId) + ext;
+  fstream studentFile(filename, ios ::in);
 
-double getBalance(int userId){
-    string ext = ".txt";
-    string filename = to_string(userId) + ext; 
-    string filepath = "accountsDatabase/" + filename;
-    fstream studentFile(filepath, ios :: in);
-    
-    if(!studentFile){
-        cout << "Error opening file!";
-    }
-    string word;
-    double balance;
-    
-    for(int i = 1; i <= 3; i++){
-        studentFile >> word >> word >> balance;
-    }
+  string word;
 
-    studentFile.close(); 
-    return balance;
+  studentFile >> word >> word >> accID;
+  for (int i = 1; i <= 1; i++) {
+    studentFile >> word >> word >> accPIN;
+  }
+
+  studentFile.close();
 }
 
-double depositMoney(double amount, int userId, int pin){
-    string ext = ".txt";
-    string filename = to_string(userId) + ext; 
-    string filepath = "accountsDatabase/" + filename;
-    ofstream studentFile(filepath);
-    
-    if(!studentFile){
-        cout << "Error opening file!";
-    }
-    string word;
-    double newBalance = balance + amount;
+// get balance func
+double getBalance(int userId) {
+  string ext = ".txt";
+  string filename = to_string(userId) + ext;
+  fstream studentFile(filename, ios ::in);
 
-    studentFile <<"ID : "<<userId<<endl;
-    studentFile <<"PIN : " << pin<<endl;
-    studentFile << "BALANCE : " << newBalance;
-    
-    studentFile.close(); 
-    return newBalance;    
-     
+  string word;
+  double balance;
+
+  for (int i = 1; i <= 3; i++) {
+    studentFile >> word >> word >> balance;
+  }
+
+  studentFile.close();
+  return balance;
 }
 
+// deposit func
+double depositMoney(double amount, int userId, int pin) {
+  string ext = ".txt";
+  string filename = to_string(userId) + ext;
+  ofstream studentFile(filename);
 
-double withdrawMoney(double amount, int userId, int pin){
-    string ext = ".txt";
-    string filename = to_string(userId) + ext; 
-    string filepath = "accountsDatabase/" + filename;
-    ofstream studentFile(filepath);
-    
-    if(!studentFile){
-        cout << "Error opening file!";
-    }
-    string word;
-    double newBalance = balance - amount;
+  string word;
+  double newBalance = balance + amount;
 
-    studentFile <<"ID : "<<userId<<endl;
-    studentFile <<"PIN : " << pin<<endl;
-    studentFile << "BALANCE : " << newBalance;
-    
-    studentFile.close(); 
-    return newBalance;    
-    
-    
+  studentFile << "ID : " << userId << endl;
+  studentFile << "PIN : " << pin << endl;
+  studentFile << "BALANCE : " << newBalance;
+
+  studentFile.close();
+  return newBalance;
 }
 
+// withdraw func
+double withdrawMoney(double amount, int userId, int pin) {
+  string ext = ".txt";
+  string filename = to_string(userId) + ext;
+  ofstream studentFile(filename);
+
+  string word;
+  double newBalance = balance - amount;
+
+  studentFile << "ID : " << userId << endl;
+  studentFile << "PIN : " << pin << endl;
+  studentFile << "BALANCE : " << newBalance;
+
+  studentFile.close();
+  return newBalance;
+}
+
+// error handler
+void clearInputBuffer() {
+  cin.get();
+  cin.clear();
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
--- class_projects/activity_1/main.cpp
+++ class_projects/activity_1/main.cpp
@@ -4,55 +4,45 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+  int x, y;
+  char answer;
 
+  while (answer != 'y' || answer != 'Y') {
+    cout << "Enter x: ";
+    cin >> x;
+    cout << "Enter y: ";
+    cin >> y;
+    cout << "\n";
 
-int main(){
-    int x, y;
-    char answer;
+    if (x == 0 && y == 0) {
+      cout << "Point (" << x << ',' << y << ") is in the Origin" << endl;
+    } else if (x > 0 && y > 0) {
+      cout << "Point (" << x << ',' << y << ") is in the 1st Quadrant" << endl;
+    } else if (x < 0 && y > 0) {
+      cout << "Point (" << x << ',' << y << ") is in the 2nd Quadrant" << endl;
+    } else if (x < 0 && y < 0) {
+      cout << "Point (" << x << ',' << y << ") is in the 3rd Quadrant" << endl;
+    } else if (x > 0 && y < 0) {
+      cout << "Point (" << x << ',' << y << ") is in the 4th Qudrant" << endl;
+    } else if (x == 0 && y >= 0 || y <= 0) {
+      cout << "Point (" << x << ',' << y << ") is in the x axis" << endl;
+    }
 
+    else if (x >= 0 || x <= 0 || y == 0) {
+      cout << "Point (" << x << ',' << y << ") is in the y axis" << endl;
+    }
 
-    while( answer != 'y' || answer != 'Y'){
-        cout << "Enter x: ";
-        cin >> x;
-        cout << "Enter y: ";
-        cin >> y;
-        cout << "\n";
+    else {
+      cout << "Invalid Input";
+    }
+    cout << "\n";
+    cout << "Do you want to enter again: [y/n]: ";
+    cin >> answer;
 
-        if( x == 0 && y == 0){
-            cout << "Point (" << x << ',' << y << ") is in the Origin" << endl;
-        }
-        else if( x > 0 && y > 0){
-            cout << "Point (" << x << ',' << y << ") is in the 1st Quadrant" << endl;
-        }
-        else if( x < 0 && y > 0){
-            cout << "Point (" << x << ',' << y << ") is in the 2nd Quadrant" << endl;
-        }
-        else if( x < 0 && y < 0){
-            cout << "Point (" << x << ',' << y << ") is in the 3rd Quadrant" << endl;
-        }
-        else if( x > 0 && y < 0){
-            cout << "Point (" << x << ',' << y << ") is in the 4th Qudrant" << endl;
-        }
-        else if( x == 0 && y >= 0 || y <= 0){
-            cout << "Point (" << x << ',' << y << ") is in the x axis" << endl;
-        }
-        
-        else if(x >= 0 || x <= 0 || y == 0){
-            cout << "Point (" << x << ',' << y << ") is in the y axis" << endl;
-        }
-        
-        
-        else{
-            cout << "Invalid Input";
-        }
-        cout << "\n";
-        cout << "Do you want to enter again: [y/n]: ";
-        cin >> answer;
-
-        if(answer == 'n' || answer == 'N') {
-            break;
-        }
-    } 
-    cout << "Program Ended";
-
+    if (answer == 'n' || answer == 'N') {
+      break;
+    }
+  }
+  cout << "Program Ended";
 }
